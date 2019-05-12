b121235fc845e51a859b64d9f7a4d3bc1a32358a
Max Wolfe <max@securitywolfe.com>
2019-05-12 01:05:33 -0400

    Add clock

--- ui/src/main.py
+++ ui/src/main.py
@@ -1,22 +1,52 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pxthon3
 import curses
+import datetime
 import yaml
 
 from collections import defaultdict
 
-COLORS = {"BLACK" : curses.COLOR_BLACK,
-          "RED" : curses.COLOR_RED,
-          "GREEN": curses.COLOR_GREEN,
-          "WHITE": curses.COLOR_WHITE,
-          "YELLOW": curses.COLOR_YELLOW,
+COLORS = {'BLACK' : curses.COLOR_BLACK,
+          'RED' : curses.COLOR_RED,
+          'GREEN': curses.COLOR_GREEN,
+          'WHITE': curses.COLOR_WHITE,
+          'YELLOW': curses.COLOR_YELLOW,
          }
-USE_COLOR = {"TEXT": (1, COLORS["WHITE"], COLORS["BLACK"]),
-             "ERROR": (2, COLORS["RED"], COLORS["BLACK"]),
-             "GOOD": (3, COLORS["GREEN"], COLORS["BLACK"]),
+USE_COLOR = {'TEXT': (1, COLORS['WHITE'], COLORS['BLACK']),
+             'ERROR': (2, COLORS['RED'], COLORS['BLACK']),
+             'GOOD': (3, COLORS['GREEN'], COLORS['BLACK']),
             }
 COLOR_PAIR = defaultdict(lambda: curses.color_pair(1))
+CLOCK = {0: ['  #####   ', ' ##   ##  ', '##     ## ', '##     ## ', '##     ## ',
+	      ' ##   ##  ', '  #####   '],
+         1: ['    ##    ', '  ####    ', '    ##    ', '    ##    ', '    ##    ',
+	       '    ##    ', '  ######  '],
+	 2: [' #######  ', '##     ## ', '       ## ', ' #######  ', '##        ',
+	       '##        ', '######### '],
+	 3: [' #######  ', '##     ## ', '       ## ', ' #######  ', '       ## ',
+	       '##     ## ', ' #######  '],
+	 4: ['##        ', '##    ##  ', '##    ##  ', '##    ##  ', '######### ',
+	       '      ##  ', '      ##  '],
+	 5: [' ######## ', ' ##       ', ' ##       ', ' #######  ', '       ## ',
+	       ' ##    ## ', '  ######  '],
+	 6: [' #######  ', '##     ## ', '##        ', '########  ', '##     ## ',
+	       '##     ## ', ' #######  '],
+	 7: [' ######## ', ' ##    ## ', '     ##   ', '    ##    ', '   ##     ',
+	       '   ##     ', '   ##     '],
+	 8: [' #######  ', '##     ## ', '##     ## ', ' #######  ', '##     ## ',
+	       '##     ## ', ' #######  '],
+	 9: [' #######  ', '##     ## ', '##     ## ', ' ######## ', '       ## ',
+	       '##     ## ', ' #######  '],
+	 ':': ['   ', '   ', ' # ', '   ', ' # ', '   ', '   '],
+        }
+GLYPH_LEN = len(CLOCK[0])
 
-BANNER_PATH = "ui/files/banner"
+BANNER_PATH = 'ui/files/banner'
+TASK_PATH = 'ui/files/tasks.yaml'
+
+
+class GlyphError(Exception):
+    pass
+
 
 def setup_colors():
     for key in USE_COLOR:
@@ -24,33 +54,116 @@ def setup_colors():
         curses.init_pair(num, fg, bg)
         COLOR_PAIR[key] = curses.color_pair(num)
 
-    COLOR_PAIR["HIGHLIGHT"] = curses.A_STANDOUT
+    COLOR_PAIR['HIGHLIGHT'] = curses.A_STANDOUT
+    COLOR_PAIR['BLINK'] = curses.A_BLINK
+    COLOR_PAIR['BOLD'] = curses.A_BOLD
+    COLOR_PAIR['DIM'] = curses.A_DIM
+    COLOR_PAIR['LINE'] = curses.A_UNDERLINE
 
-def print_banner(stdscr):
+def _combine_glyphs(orig, to_add):
+    if len(orig) != len(to_add):
+        raise GlyphError('Glyph Lengths are not equal')
+    
+    for i, g in enumerate(to_add):
+        orig[i] += g
+
+def _val_to_glyph(num):
+    digits = []
+
+    if num > 10:
+        digits.append(num // 10)
+        num %= 10
+    digits.append(num)
+
+    glyphs = [''] * GLYPH_LEN
+    for digit in digits:
+        glyph = CLOCK[digit]
+        _combine_glyphs(glyphs, glyph)
+
+    return glyphs
+
+def _get_time():
+    time_str = str(datetime.datetime.now().time())
+    return time_str.split(':')[:2]
+
+def _determine_clock():
+    hours, minutes = _get_time()
+    hours, minutes = int(hours), int(minutes)
+
+    # Default
+    color = COLOR_PAIR['GOOD']
+
+    # After Hours, Go Home!
+    if hours < 9 or hours > 6:
+        color = COLOR_PAIR['ERROR']
+
+    # TODO: Add Meeting Now / Meeting Soon
+    
+    return color
+
+def print_clock(stdscr, y, x):
+    clock_color = _determine_clock()
+    hours, minutes = _get_time()
+
+    hour = int(hours) % 12
+    if hour == 0:
+        hour = 12
+    time_glyphs = _val_to_glyph(hour)
+
+    _combine_glyphs(time_glyphs, CLOCK[':'])
+    
+    minute = int(minutes) % 60
+    if minute < 10:
+        _combine_glyphs(time_glyphs, CLOCK[0])
+    _combine_glyphs(time_glyphs, _val_to_glyph(minute))
+
+    x //= 2
+    x -= (len(time_glyphs[0]) - 1) // 2
+    for minus, line in enumerate(reversed(time_glyphs), 1):
+        stdscr.addstr(y - minus, x, line, clock_color)
+
+    return y
+
+
+def print_banner(stdscr, y, x):
     with open(BANNER_PATH, 'r') as f:
         banner = f.read()
 
-    for x, line in enumerate(banner.splitlines()):
-        stdscr.addstr(x, 0, line, COLOR_PAIR["TEXT"])
+    for y, line in enumerate(banner.splitlines(), y):
+        stdscr.addstr(y, x, line, COLOR_PAIR['BOLD'])
 
-    return x
+    return y
 
-def print_tasks(stdscr, x, y):
-    stdscr.addstr(x, y, "First Line!", COLOR_PAIR["GOOD"])
-    stdscr.addstr(x + 1, y, "Bad Line!", COLOR_PAIR["BAD"])
-    stdscr.addstr(x + 2, y, "Error Line!", COLOR_PAIR["ERROR"])
-    stdscr.addstr(x + 3, y, "Highlight Line!", COLOR_PAIR["HIGHLIGHT"])
+def _get_task_list():
+    task_dict = {}
+    with open(TASK_PATH, 'r') as f:
+        task_dict = yaml.safe_load(f)
+    
 
+
+def print_tasks(stdscr, y, x):
+    task_list = _get_task_list()
+    task_list = [('First Line!', COLOR_PAIR['GOOD']),
+                 ('Bad Line!', COLOR_PAIR['BAD']),
+                 ('Error Line!', COLOR_PAIR['ERROR']),
+                 ('Highlight Line!', COLOR_PAIR['HIGHLIGHT']),
+                ]
+
+    for y, (task_str, task_attr) in enumerate(task_list, y):
+        stdscr.addstr(y, x, task_str, task_attr)
+    return y
 
 def main(stdscr):
     setup_colors()
 
+    max_y, max_x = stdscr.getmaxyx()
+
     # Give line space
-    next_line = print_banner(stdscr) + 2
+    next_line = print_banner(stdscr, 0, 0) + 2
 
     print_tasks(stdscr, next_line, 0)
 
-
+    print_clock(stdscr, max_y, max_x)
 
     stdscr.getch()
 
